c314a8a107855eca15ff794ed2bd45def724827f
Enrique Juarez <enriquejuarez.ej7@gmail.com>
2024-04-30 01:08:13 -0600

    modificadores get & set

--- MainPersona.java
+++ MainPersona.java
@@ -1,25 +1,48 @@
 import java.util.Scanner;
+
 public class MainPersona {
-public static void main(String[] args) {
-Scanner per1 = new Scanner(System.in);
-System.out.println("ingresa tu nombre:");
-String nombre=per1.next();
-System.out.println("Escribe tu apellido");
-String apellido=per1.next();
-System.out.println("ingresa tu edad");
-int edad=per1.nextInt();
-System.out.println("Escribe tu altura");
-double altura=per1.nextDouble();
+    public static void main(String[] args) {
+        Scanner per1 = new Scanner(System.in);
+        System.out.println("ingresa tu nombre:");
+        String nombre = per1.next();
+        System.out.println("Escribe tu apellido");
+        String apellido = per1.next();
+        System.out.println("ingresa tu edad");
+        int edad = per1.nextInt();
+        System.out.println("Escribe tu altura");
+        double altura = per1.nextDouble();
 
-Persona obj = new Persona ();
-obj.nombre=nombre;
-obj.apellido=apellido;
-obj.edad=edad;
-obj.altura=altura;
+        Persona obj = new Persona();
+        obj.setNombre(nombre);
+        obj.setApellido(apellido);
+        obj.setEdad(edad);
+        obj.setAltura(altura);
 
-obj.mostrarNombre();
-obj.mostrarApellido();
-obj.mostrarEdad();
-obj.mostrarAltura();
+        obj.getNombre();
+        obj.getApellido();
+        obj.getEdad();
+        obj.getAltura();
+        System.out.println(obj.toString());
+
+        Persona per2 = new Persona();
+        System.out.println("ingresa tu nombre:");
+        String nombre2 = per1.next();
+        System.out.println("Escribe tu apellido");
+        String apellido2 = per1.next();
+        System.out.println("ingresa tu edad");
+        int edad2 = per1.nextInt();
+        System.out.println("Escribe tu altura");
+
+        double altura2 = per1.nextDouble();
+        per2.setNombre(nombre2);
+        per2.setApellido(apellido2);
+        per2.setEdad(edad2);
+        per2.setAltura(altura2);
+        
+        per2.getNombre();
+        per2.getApellido();
+        per2.getEdad();
+        per2.getAltura();
+        System.out.println(per2.toString());
     }
 }
--- Persona.java
+++ Persona.java
@@ -1,18 +1,49 @@
 public class Persona {
-    String nombre;
-    String apellido;
-    int edad;
-    double altura;
-    public void mostrarNombre(){
-    System.out.println("Mi nombre es: "+this.nombre);
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private double altura;
+
+    public String getNombre() {
+        return nombre;
     }
-    public void mostrarApellido(){
-    System.out.println("Mi apellido es: "+this.apellido);
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public void mostrarEdad(){
-    System.out.println("Mi edad es: "+this.edad);
+
+    public String getApellido() {
+        return apellido;
     }
-    public void mostrarAltura(){
-    System.out.println("Mi altura es: "+this.altura);
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad < 0) {
+            System.out.println("la edad es invalida");
+        } else
+            this.edad = edad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            System.out.println("La altura es invalida");
+        } else
+            this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", altura=" + altura + "]";
     }
 }
